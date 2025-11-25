e60b41fb2f43b1130bc39e8a6f4a894037f57da4
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-11-24 23:37:03 -0500

    It is just a proof

--- Hello.py
+++ Hello.py
@@ -1,4 +1,2 @@
 print("Hello, world!")
 print('This is the first commit for this new adventure in Machine Learning.')
-print('This is the first commit for this new adventure in Machine Learning.')
-print('This is the first commit for this new adventure in Machine Learning.')
