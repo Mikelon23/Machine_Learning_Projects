7b8a4f5c9adc655025a21e71b8febeb6b792984a
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-11-29 23:56:24 -0500

    Python implementation of a Perceptron

--- First_Neural_Network.py
+++ First_Neural_Network.py
@@ -2,4 +2,7 @@ import random
 
 # 1. The Data (The Fuel) - OR Gate Logic
 # This defines the reality we want the machine to learn.
-
+training_data = [
+    {'inputs': [0, 0], 'label': 0},
+    {'inputs': [0, 1], 'label': 1},
+    
