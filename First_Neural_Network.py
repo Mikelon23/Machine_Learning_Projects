b9e5d99ff09c27fe2494c11cf436a50be6f19db1
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-11-30 00:02:24 -0500

    Python implementation of a Perceptron

--- First_Neural_Network.py
+++ First_Neural_Network.py
@@ -32,4 +32,9 @@ for epoch in range(epochs):
         inputs = data['inputs']
         target = data['label']
 
+        # A. Forward Pass (The Attempt)
+        # Formula: z = (x1 * w1) + (x2 * w2) + b
+        weighted_sum = (inputs[0] * weights[0]) + (inputs[1] * weights[1]) + bias
+        prediction = activation_function(weighted_sum)
+
         
