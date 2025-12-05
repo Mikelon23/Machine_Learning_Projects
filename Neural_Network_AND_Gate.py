b73c2e312c8fa26951c47d37ef8848cabe7b0bba
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-12-05 00:17:33 -0500

    This exercise is the same that First_Neural_Network_OR_Gate.py, but with other data to proof AND Gate

--- Neural_Network_AND_Gate.py
+++ Neural_Network_AND_Gate.py
@@ -7,3 +7,10 @@ random.seed(42)
 # Notice: The code logic (Algorithm) remains exactly the same. 
 # We only change the Data (The Fuel).
 # AND Logic: Output 1 only if BOTH inputs are 1.
+training_data = [
+    {'inputs': [0, 0], 'label': 0},
+    {'inputs': [0, 1], 'label': 0}, # Changed from 1 to 0
+    {'inputs': [1, 0], 'label': 0}, # Changed from 1 to 0
+    {'inputs': [1, 1], 'label': 1},
+]
+
