88712931973ae08a55d466f0171076e1d6bc2c81
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-12-06 23:58:20 -0500

    This exercise is the same that First_Neural_Network_OR_Gate.py, but with other data to proof AND Gate

--- Neural_Network_AND_Gate.py
+++ Neural_Network_AND_Gate.py
@@ -57,4 +57,3 @@ for epoch in range(20):
 print("\nVerification (AND Gate):")
 for data in training_data:
     val = (data['inputs'][0] * weights[0]) + (data['inputs'][1] * weights[1]) + bias
-    print(f"{data['inputs']} -> {activation_function(val)} (Expected: {data['label']})")
