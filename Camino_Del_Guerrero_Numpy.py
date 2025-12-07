971202233159e14ff912110140cf4c34bfa1980e
Mikelon23 <miguel.tierra@espoch.edu.ec>
2025-12-07 01:19:06 -0500

    Ejercicio de codigo de Backpropagation desde cero en Python

--- Camino_Del_Guerrero_Numpy.py
+++ Camino_Del_Guerrero_Numpy.py
@@ -3,3 +3,7 @@ import numpy as np
 # Semilla para reproducibilidad
 np.random.seed(42)
 
+# ==========================================
+# 1. PREPARACIÓN DE DATOS (XOR)
+# ==========================================
+# Inputs (4 ejemplos, 2 características cada uno)
